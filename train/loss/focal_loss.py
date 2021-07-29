0c7a06b3671a6ca1249cad43d42e072c63aa3bb7
Hyeonchul Jung <gus8cjf@naver.com>
2021-07-29 20:05:54 +0900

    Create focal_loss.py

---
+++ train/loss/focal_loss.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.cuda.amp as amp
+
+class FocalSigmoidLossFuncV2(torch.autograd.Function):
+    '''
+    compute backward directly for better numeric stability
+    '''
+    @staticmethod
+    @amp.custom_fwd(cast_inputs=torch.float32)
+    def forward(ctx, logits, label, alpha, gamma):
+        #  logits = logits.float()
+
+        probs = torch.sigmoid(logits)
+        coeff = (label - probs).abs_().pow_(gamma).neg_()
+        log_probs = torch.where(logits >= 0,
+                F.softplus(logits, -1, 50),
+                logits - F.softplus(logits, 1, 50))
+        log_1_probs = torch.where(logits >= 0,
+                -logits + F.softplus(logits, -1, 50),
+                -F.softplus(logits, 1, 50))
+        ce_term1 = log_probs.mul_(label).mul_(alpha)
+        ce_term2 = log_1_probs.mul_(1. - label).mul_(1. - alpha)
+        ce = ce_term1.add_(ce_term2)
+        loss = ce * coeff
+
+        ctx.vars = (coeff, probs, ce, label, gamma, alpha)
+
+        return loss
+
+    @staticmethod
+    @amp.custom_bwd
+    def backward(ctx, grad_output):
+        '''
+        compute gradient of focal loss
+        '''
+        (coeff, probs, ce, label, gamma, alpha) = ctx.vars
+
+        d_coeff = (label - probs).abs_().pow_(gamma - 1.).mul_(gamma)
+        d_coeff.mul_(probs).mul_(1. - probs)
+        d_coeff = torch.where(label < probs, d_coeff.neg(), d_coeff)
+        term1 = d_coeff.mul_(ce)
+
+        d_ce = label * alpha
+        d_ce.sub_(probs.mul_((label * alpha).mul_(2).add_(1).sub_(label).sub_(alpha)))
+        term2 = d_ce.mul(coeff)
+
+        grads = term1.add_(term2)
+        grads.mul_(grad_output)
+
+        return grads, None, None, None
+
+
+class FocalLossV2(nn.Module):
+
+    def __init__(self,
+                 alpha=0.25,
+                 gamma=2,
+                 reduction='mean'):
+        super(FocalLossV2, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, logits, label):
+        loss = FocalSigmoidLossFuncV2.apply(logits, label, self.alpha, self.gamma)
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        if self.reduction == 'sum':
+            loss = loss.sum()
+        return loss
+
+
+if __name__ == "__main__":
+    pred = torch.randn(32, 51)
+    target = torch.ones_like(pred)
+
+    focal_loss = FocalLossV2()
+    loss = focal_loss(pred, target)
+    print(loss)
